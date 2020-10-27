24fbd5bb09d28eeb6b85c31e458f2b9b7a54c3bf
Liwei Zhang <16180088+Cornelllz348@users.noreply.github.com>
2020-10-26 23:52:28 -0400

    Update solution_31-40.py

--- Leetcode/solution_31-40.py
+++ Leetcode/solution_31-40.py
@@ -34,3 +34,55 @@ def searchInsert(self, nums, target):
 # Space: O(1)
 
 # -----
+
+
+
+# -----
+
+# 38. Count and Say *
+
+# The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+# countAndSay(1) = "1"
+# countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1), 
+# which is then converted into a different digit string.
+# To determine how you "say" a digit string, split it into the minimal number of groups 
+# so that each group is a contiguous section all of the same character. 
+# Then for each group, say the number of characters, then say the character. 
+# To convert the saying into a digit string, replace the counts with a number and concatenate every saying.
+# Given a positive integer n, return the nth term of the count-and-say sequence.
+
+def countAndSay(self, n):
+    """
+    :type n: int
+    :rtype: str
+    """
+    # start with base case '1'
+    return ''.join(self.nextSeq(n, ['1', 'E']))
+
+def nextSeq(self, n, prevSeq):
+    # if n == 1, return the subsequence of all but the last digit ('E')
+    if n == 1:
+        return prevSeq[:-1]
+    # construct the next sequence from the previous sequence
+    nextSeq = []
+    prevDigit = prevSeq[0]
+    count = 1
+    for digit in prevSeq[1:]:
+        if digit == prevDigit:
+            count += 1
+        else:
+            nextSeq.extend([str(count), prevDigit])
+            prevDigit = digit
+            count = 1
+
+    nextSeq.append('E')
+
+    return self.nextSeq(n-1, nextSeq)
+
+# Method: String
+# Time: O(2^n)
+# Space: O(2^(n-1))
+
+# -----
+
+
