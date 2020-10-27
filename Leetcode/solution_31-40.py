e1cf439c9947b059792c0f4114da6d3d280fe224
Liwei Zhang <16180088+Cornelllz348@users.noreply.github.com>
2020-10-27 19:45:10 -0400

    Update solution_31-40.py

--- Leetcode/solution_31-40.py
+++ Leetcode/solution_31-40.py
@@ -1,4 +1,50 @@
+# 31. Next Permutation **
 
+# Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers
+# If such an arrangement is not possible, it must rearrange it as the lowest possible order (i.e., sorted in ascending order)
+# The replacement must be in place and use only constant extra memory
+
+def nextPermutation(nums):
+    """
+    :type nums: List[int]
+    :rtype: None Do not return anything, modify nums in-place instead.
+    """
+    # find a pivot index such that nums[pivot-1] < num[pivot]
+    # i.e. this is the first element counting from the end of the list that is non-ascending
+    # if pivot is 0 meaning the list is descending, there is no larger permutation than the current
+    pivot = len(nums) - 2
+    while (pivot >= 0 and nums[pivot+1] <= nums[pivot]):
+            pivot = pivot - 1
+    
+    # find the smallest element after pivot that is larger than the pivot element and swap
+    if pivot >= 0:
+        j = len(nums) - 1
+        while (j >= 0 and nums[j] <= nums[pivot]):
+            j = j - 1
+        swap(nums, pivot, j)
+    
+    # if the entire list is descending, reverse the entire list
+    # otherwise 
+    reverse(nums, pivot+1)
+
+# swap two elements in a list
+def swap(nums, i, j):
+    temp = nums[i]
+    nums[i] = nums[j]
+    nums[j] = temp
+
+# reverse the end of the list starting from the element start   
+def reverse(nums, start):
+    i = start
+    j = len(nums)-1
+    while i < j:
+        swap(nums, i, j)
+        i = i + 1
+        j = j - 1
+        
+# Method: Array
+# Time: O(n)
+# Space: O(1)
 
 # -----
 
