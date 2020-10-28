77cc3a59e2b371e04e88ba2e8e867e6dcf8d99ba
Liwei Zhang <16180088+Cornelllz348@users.noreply.github.com>
2020-10-27 23:17:49 -0400

    Update solution_31-40.py

--- Leetcode/solution_31-40.py
+++ Leetcode/solution_31-40.py
@@ -87,6 +87,46 @@ def search(nums, target):
 
 # -----
 
+# 34. Find First and Last Position of Element in Sorted Array **
+
+# Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+# If target is not found in the array, return [-1, -1].
+
+def searchRange(nums, target):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: List[int]
+    """
+    result = [-1] * 2
+    if nums == None or len(nums) == 0:
+        return result
+    left, right = 0, len(nums) - 1
+    index = 0
+    while left <= right:
+        mid = left + (right - left) // 2
+        if target == nums[mid]:
+            index = mid
+            break
+        elif target < nums[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+    p1 = p2 = index
+    while p1 > -1 and nums[p1] == target:
+        result[0] = p1
+        p1 -= 1
+    while p2 < len(nums) and nums[p2] == target:
+        result[1] = p2
+        p2 += 1
+    return result
+
+# Method: Binary Search
+# Time: O(logN)
+# Space: O(1)
+
+# -----
+
 # 35. Search Insert Position *
 
 # Given a sorted array of distinct integers and a target value, return the index if the target is found. 
