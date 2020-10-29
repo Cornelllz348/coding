ca5bd5ab475c3fddf332821ac7b2919f1e4917c0
Liwei Zhang <16180088+Cornelllz348@users.noreply.github.com>
2020-10-28 21:58:29 -0400

    Update solution_31-40.py

--- Leetcode/solution_31-40.py
+++ Leetcode/solution_31-40.py
@@ -160,6 +160,41 @@ def searchInsert(self, nums, target):
 
 # -----
 
+# 36. Valid Sudoku **
+
+# Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+# Each row must contain the digits 1-9 without repetition.
+# Each column must contain the digits 1-9 without repetition.
+# Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+def isValidSudoku(self, board):
+    """
+    :type board: List[List[str]]
+    :rtype: bool
+    """
+    output = True
+    row = [[] for i in range(9)]
+    col = [[] for i in range(9)]
+    grid = [[] for i in range(9)]
+    for i in range(9):
+        for j in range(9):
+            value = board[i][j]
+            igrid = j // 3 * 3 + i // 3
+            if value != '.' and (value in row[i] or value in col[j] or value in grid[igrid]):
+                output = False
+                break
+            elif value != '.':
+                row[i].append(value)
+                col[j].append(value)
+                grid[igrid].append(value)
+    return output
+
+# Method: HashSet
+# Time: O(1)
+# Space: O(1)
+
+# -----
+
 
 
 # -----
