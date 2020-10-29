d4b6ac4e7325d04d9f922848462f10183b68eeab
Liwei Zhang <16180088+Cornelllz348@users.noreply.github.com>
2020-10-28 22:41:41 -0400

    Update solution_31-40.py

--- Leetcode/solution_31-40.py
+++ Leetcode/solution_31-40.py
@@ -245,4 +245,48 @@ def nextSeq(self, n, prevSeq):
 
 # -----
 
+# 39. Combination Sum **
+
+# Given an array of distinct integers candidates and a target integer target, 
+# return a list of all unique combinations of candidates where the chosen numbers sum to target. 
+# You may return the combinations in any order.
+# The same number may be chosen from candidates an unlimited number of times. 
+# Two combinations are unique if the frequency of at least one of the chosen numbers is different.
+# It is guaranteed that the number of unique combinations that sum up to target is less than 150 combinations 
+# for the given input.
+
+def combinationSum(self, candidates, target):
+    """
+    :type candidates: List[int]
+    :type target: int
+    :rtype: List[List[int]]
+    """
+
+    result = []
+
+    def backtrack(remain, combination, start):
+        # if remain = 0, append the combination as one of the solutions
+        if remain == 0:
+            result.append(list(combination))
+        # if remain < 0, stop current exploration
+        elif remain < 0:
+            return
+        for i in range(start, len(candidates)):
+            # append the number into the combination
+            combination.append(candidates[i])
+            # give the current number another chance rather than moving on
+            backtrack(remain - candidates[i], combination, i)
+            # remove the number from the combination
+            combination.pop()
+
+    backtrack(target, [], 0)
+
+    return result
+
+# Method: DFS
+# Time: O(N^(T/M+1)) where N is number of candidates, T is target value, and M is the min value among candidates
+# Space: O(T/M)
+
+# -----
+
 
