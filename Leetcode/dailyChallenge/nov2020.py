483100f68a1130cf32dfdc74e845154ba374b29a
Liwei Zhang <16180088+Cornelllz348@users.noreply.github.com>
2020-11-06 19:26:48 -0500

    Update nov2020.py

--- Leetcode/dailyChallenge/nov2020.py
+++ Leetcode/dailyChallenge/nov2020.py
@@ -1,4 +1,4 @@
-# 252. Meeting Rooms I *
+# NOV 1: 252. Meeting Rooms I *
 
 # Given an array of meeting time intervals where intervals[i] = [starti, endi], 
 # determine if a person could attend all meetings.
@@ -28,7 +28,11 @@ def canAttendMeetings(intervals):
 
 # -----
 
-# 1290. Convert Binary Number in a Linked List *
+# NOV 2: 1290. Convert Binary Number in a Linked List *
+
+# Given head which is a reference node to a singly-linked list. 
+# The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+# Return the decimal value of the number in the linked list.
 
 def getDecimalValue(head):
     """
@@ -65,7 +69,7 @@ def getDecimalValue(head):
 
 # -----
 
-# 1446. Consecutive Characters
+# NOV 4: 1446. Consecutive Characters
 
 # Given a string s, the power of the string is the maximum length of a non-empty substring that 
 # contains only one unique character.
@@ -92,6 +96,41 @@ def maxPower(s):
 
 # Method: Two Pointers
 # Time: O(N)
+# Space: O(1)
+
+# -----
+
+# NOV 7: 1283. Find the Smallest Divisor Given a Threshold
+
+# Given an array of integers nums and an integer threshold, we will choose a positive integer divisor and divide all the array by it and sum the result of the division. 
+# Find the smallest divisor such that the result mentioned above is less than or equal to threshold.
+# Each result of division is rounded to the nearest integer greater than or equal to that element. (For example: 7/3 = 3 and 10/2 = 5).
+# It is guaranteed that there will be an answer.
+
+def smallestDivisor(self, nums, threshold):
+    """
+    :type nums: List[int]
+    :type threshold: int
+    :rtype: int
+    """
+    nums.sort()
+    left = 1
+    right = nums[-1]
+    result = nums[-1]
+    while left <= right:
+        mid = left + (right - left) // 2
+        total = 0
+        for i in range(len(nums)):
+            total += nums[i] // mid + (nums[i] % mid > 0)
+        if total > threshold:
+            left = mid + 1
+        else:
+            result = min(result, mid)
+            right = mid - 1
+    return result
+
+# Method: Binary Search
+# Time: O(Nlogn) where N is size of nums and n is the max integer in nums
 # Space: O(1)
 
 # -----
