7896a21215ba596ccdeeed276d2887f814ef0e4c
Liwei Zhang <16180088+Cornelllz348@users.noreply.github.com>
2020-11-07 23:49:40 -0500

    Update solution_41-50.py

--- Leetcode/solution_41-50.py
+++ Leetcode/solution_41-50.py
@@ -1,3 +1,6 @@
+
+# -----
+
 # 43. Multiply Strings
 
 # Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
@@ -38,3 +41,6 @@ def multiply(num1, num2):
     else:
         string = ''.join(map(str, result))
     return string
+
+# -----
+
