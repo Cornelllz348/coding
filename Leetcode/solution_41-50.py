c8c7002314809778aa95e06cf4c16ddfdb4316fd
Liwei Zhang <16180088+Cornelllz348@users.noreply.github.com>
2020-11-07 23:48:48 -0500

    Update solution_41-50.py

--- Leetcode/solution_41-50.py
+++ Leetcode/solution_41-50.py
@@ -3,7 +3,7 @@
 # Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
 # Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
 
-def multiply(self, num1, num2):
+def multiply(num1, num2):
     """
     :type num1: str
     :type num2: str
