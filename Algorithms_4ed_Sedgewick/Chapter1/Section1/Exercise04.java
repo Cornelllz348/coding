416cd028ca1000cd35bd4a23ad481c35dd7e6290
Liwei Zhang <16180088+Cornelllz348@users.noreply.github.com>
2021-12-03 12:06:59 -0500

    Add files via upload

---
+++ Algorithms_4ed_Sedgewick/Chapter1/Section1/Exercise04.java
@@ -0,0 +1,14 @@
+package chapter0101;
+
+import edu.princeton.cs.algs4.*;
+
+public class Exercise04
+{
+	public static void main(String[] args)
+	{
+		StdOut.println("a. if (a > b) then c = 0 -> if...then is wrong");
+		StdOut.println("b. if a > b { c = 0; } -> the \";\" is wrong");
+		StdOut.println("c. if (a > b) c = 0; -> correct" );
+		StdOut.println("d. if (a > b) c = 0 else b = 0 -> correct");
+	}
+}
